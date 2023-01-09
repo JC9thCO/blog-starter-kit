edbadf72169dfd37f49279dbbaa7a6b5b0767222
Justin Cook <98044444+JC9thCO@users.noreply.github.com>
2023-01-09 19:00:11 +0000

    Adding GTM proper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import { AppProps } from 'next/app'
+import { analytics } from '../components/analytics';
+import { ReactElement, ReactNode, useEffect } from 'react'
 import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    analytics(window, document, 'script', 'dataLayer', 'GTM-TVF5LJ7');
+  })
+  
   return <Component {...pageProps} />
 }
+
