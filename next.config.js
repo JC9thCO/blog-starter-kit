22dbbc4e01d6be7b2942878227cc07208b9142f3
Justin Cook <98044444+JC9thCO@users.noreply.github.com>
2023-01-09 18:17:42 +0000

    Updates to the config

--- next.config.js
+++ next.config.js
@@ -9,6 +9,21 @@
             destination: '/posts/auto-cpanel-backup',
             permanent: true,
           },
+          {
+            source: '/2006/11/28/convert-an-ip-address-to-ip-number-with-php-asp-c-and-vbnet/',
+            destination: '/posts/convert-ip-address',
+            permanent: true,
+          },
+          {
+            source: '/2006/07/17/upload-multiple-files-at-once-with-php/',
+            destination: '/posts/upload-multiple-files',
+            permanent: true,
+          },
+          {
+            source: '/2006/03/31/php-parse-a-string-between-two-strings/',
+            destination: '/posts/php-parse-between-strings',
+            permanent: true,
+          },
         ]
       },
   }
